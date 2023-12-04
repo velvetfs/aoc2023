d19144be4e377258a1d197e80dbf0f9bed811f5d
RosyGraph <chris.stevenson@live.com>
2023-12-04 10:23:50 -0700

    Create `data EnginePart`

--- chris/day03/app/Main.hs
+++ chris/day03/app/Main.hs
@@ -1,5 +1,19 @@
 module Main where
 
+data EnginePart =
+  EnginePart
+    { number        :: Int
+    , rowIndex      :: Int
+    , startColIndex :: Int
+    , endColIndex   :: Int
+    } deriving (Show)
+
+data Coord =
+  Coord
+    { x :: Int
+    , y :: Int
+    } deriving (Show)
+
 main :: IO ()
 main = do
   part1
@@ -8,8 +22,18 @@ part1 :: IO ()
 part1 = do
   content <- readFile "in/ex1.in"
   let ls = lines content
-  let result = map numberSequences ls
+  let seq = map numberSequences ls
+  let result = map matrixEnum seq
   print result
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [1..]
+
+matrixEnum :: Int -> [a] -> [(Int, Int, a)]
+matrixEnum i s = map (\x -> addRow (i, x)) s
+
+addRow :: Int -> (Int, a) -> (Int, Int, a)
+addRow i t = (i, fst t, snd t)
 
 numberSequences :: String -> [String]
 numberSequences s
@@ -23,7 +47,6 @@ breakOffNumber [] = ([], [])
 breakOffNumber (c:cs)
   | isNotNumber c = ([], cs)
   | otherwise      = let (prefix, suffix) = breakOffNumber cs in (c : prefix, suffix)
-
 
 isNotNumber :: Char -> Bool
 isNotNumber c = not (c `elem` ['0'..'9'])
